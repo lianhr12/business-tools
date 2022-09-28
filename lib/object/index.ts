74285faec8190c60d4cb1ec5c18b59743c62430a
hrope <lhr9311@gmail.com>
2022-09-28 17:32:57 +0800

    新增：添加object相关操作方法

---
+++ lib/object/index.ts
@@ -0,0 +1,45 @@
+/**
+ * @desc   判断`obj`是否为空
+ * @param  {Object} obj
+ * @return {Boolean}
+ */
+export function isEmptyObject(obj) {
+  if (!obj || typeof obj !== 'object' || Array.isArray(obj))
+      return false
+  return !Object.keys(obj).length
+}
+
+/**
+ * @desc 深拷贝，支持常见类型
+ * @param {Any} values
+ * @return {Any}
+ */
+export function deepClone(values) {
+  let copy;
+
+  if (null == values || "object" != typeof values) return values;
+
+  if (values instanceof Date) {
+      copy = new Date();
+      copy.setTime(values.getTime());
+      return copy;
+  }
+
+  if (values instanceof Array) {
+      copy = [];
+      for (let i = 0, len = values.length; i < len; i++) {
+          copy[i] = deepClone(values[i]);
+      }
+      return copy;
+  }
+
+  if (values instanceof Object) {
+      copy = {};
+      for (const attr in values) {
+          if (values.hasOwnProperty(attr)) copy[attr] = deepClone(values[attr]);
+      }
+      return copy;
+  }
+
+  throw new Error("Unable to copy values! Its type isn't supported.");
+}
